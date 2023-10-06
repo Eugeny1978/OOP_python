b214718d489393878136a96c89ec040d1edbe4bd
Eugeny1978 <luchnik78@mail.ru>
2023-10-06 13:47:35 +0300

    Add video_36

--- lessons/video_36.py
+++ lessons/video_36.py
@@ -17,14 +17,25 @@ class Women1(metaclass=Meta):
 # МетаКласс Meta превращает класс Women1 в Women2:
 
 class Women2:
+    title = 'Header'
+    content = 'any content'
+    photo = 'path to photo'
     class_attrs = {'title': 'Header', 'content': 'any content', 'photo': 'path to photo'}
     def __init__(self, *args, **kwargs):
         for key, value in self.class_attrs.items():
             self.__dict__[key] = value
 
+class Women3:
+    title = 'Header'
+    content = 'any content'
+    photo = 'path to photo'
+
 # --- RUN ---------------------------------------
 
 w1 = Women1()
 w2 = Women2()
+w3 = Women3()
 print(w1.__dict__, '\n')
-print(w2.__dict__)
+print(w2.__dict__, '\n')
+print(w3.__dict__)
+print(w3.title, w3.content, w3.photo, sep=' | ')
